07af3d8f9dd058b62ed70ad3fb69332217bafdeb
MONSTER_SECURITY <69053807+MONSTER-hp@users.noreply.github.com>
2021-09-06 11:38:49 +0430

    Update MONSTER_IR.py

--- MONSTER_IR.py
+++ MONSTER_IR.py
@@ -1,9 +1,15 @@
 import requests as req
 from colorama import Fore
 from time import sleep
-print(Fore.CYAN+"""
-hello monster
-""")
+print("")
+print("")
+print("   \  |   _ \    \  |   ___| __ __|  ____|   _ \    _ _|   _ \  ")
+print("  |\/ |  |   |    \ | \___ \    |    __|    |   |     |   |   | ")
+print("  |   |  |   |  |\  |       |   |    |      __ <      |   __ <  ")
+print(" _|  _| \___/  _| \_| _____/   _|   _____| _| \_\   ___| _| \_\ ")
+print("                                                _____|          ")
+print("")
+print("")
 MONSTER_hp=input(Fore.GREEN+"Enter"+Fore.RED+"Number irancell"+Fore.GREEN+"=>")
 print(Fore.GREEN+"please wait ...")
 url=str("https://ziroapi.xyz/Apis/irancell/?phone="+MONSTER_hp)
